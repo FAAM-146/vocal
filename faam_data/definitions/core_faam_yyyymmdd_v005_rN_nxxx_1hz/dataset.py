2964c78db0b6ed0086db4b91dde06f82af7a731c
Dave Sproson <daspr@faam.ac.uk>
2021-12-16 12:45:28 +0000

    Added DatasetMeta to Dataset instances.

--- faam_data/definitions/core_faam_yyyymmdd_v005_rN_nxxx_1hz/dataset.py
+++ faam_data/definitions/core_faam_yyyymmdd_v005_rN_nxxx_1hz/dataset.py
@@ -1,9 +1,16 @@
 from .variables import variables
 from ..dimensions import Time
-from ...dataset import Dataset
+from ...dataset import Dataset, DatasetMeta
 from ...definitions import global_attribute_factory
 
+
+meta = DatasetMeta(
+    file_pattern = 'core_faam_[0-9]{8}_v005_r[0-9]+_[a-z][0-9]{3}.nc'
+
+)
+
 dataset = Dataset.construct(
+    meta=meta,
     dimensions=[Time],
     attributes=global_attribute_factory(),
     variables=variables
