14537ccadc352a49fb3217b189a249c4fa037ffe
Dave Sproson <daspr@faam.ac.uk>
2021-12-16 12:37:59 +0000

    Add metadata model for datasets.

--- faam_data/dataset.py
+++ faam_data/dataset.py
@@ -1,17 +1,22 @@
 from __future__ import annotations
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from .attributes import GlobalAttributes
 from .group import Group
 from .variable import Variable
 
 
+class DatasetMeta(BaseModel):
+    file_pattern: str = Field(description='Canonical filename pattern for this dataset')
+
+
 class Dataset(BaseModel):
     class Config:
         title = 'FAAM Dataset Schema'
 
+    meta: DatasetMeta
     attributes: GlobalAttributes
     groups: Optional[Group]
     variables: list[Variable]
