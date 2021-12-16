95a550ed59c08dc2f0e9b3074524e36ca29caba0
Dave Sproson <daspr@faam.ac.uk>
2021-12-16 19:12:17 +0000

    Added examples to required global attribute defs.

--- faam_data/attributes/globals.py
+++ faam_data/attributes/globals.py
@@ -17,53 +17,245 @@ class GlobalAttributes(BaseModel):
     class Config:
         title = 'FAAM Global Metadata Schema'
    
-    Conventions: str = Field(description="The conventions followed by this data", regex='CF-1.9,?\s?ACDD-1.3')
-    acknowledgement: str = Field(description=f'The acknowledgement for use of this data. Should read: {ACKNOWLEDGEMENT}')
-    creator_address: str = Field(description=f'The address of the FAAM offices. Should read: {CREATOR_ADDRESS}')
-    creator_email: str = Field(description='The email address of the data creator, or a generic FAAM email.', regex='.+@.+')
-    creator_institution: str = Field(description=f'The institute responsible for the creation of the data. Should read: {CREATOR_INSTITUTION}')
-    creator_name: str = Field(description='The name of the person or institute responsible for this data')
-    creator_type: str = Field(description=f'The type of creator. Should be one of {", ".join(CREATOR_TYPES)}')
-    date: datetime.date = Field(description='Date of the flight')
-    date_created: datetime.datetime = Field(description='ISO:8601 date/time file created, ideally yyyy-mm-ddTHH:MM:SSZ. Equivalent to revision_date.')
-    flight_date: datetime.date = Field(description='Date of the flight')
-    flight_number: str = Field(description='The flight number', regex='[a-z][0-9]{3}')
-    geospatial_bounds: str = Field(description='A well-known text representation of the flight envelope')
-    geospatial_bounds_crs: str = Field(description='Coordinate reference system for geospatial attributes.', regex='EPSG:[0-9]+')
-    geospatial_lat_max: float = Field(ge=-90, le=90, description='Maximum latitude of the flight.')
-    geospatial_lat_min: float = Field(ge=-90, le=90, description='Minimum latitude of the flight.')
-    geospatial_lat_units: str = Field(description='The units for geospatial_lat_* attributes')
-    geospatial_lon_max: float = Field(description='Maximum longitude of the flight', gt=-360, lt=360)
-    geospatial_lon_min: float = Field(description='Minimum longitude of the flight', gt=-360, le=360)
-    geospatial_vertical_max: float = Field(description='The maximum altitude of the flight')
-    geospatial_vertical_min: float = Field(description='The minimum altitude of the flight')
-    geospatial_vertical_units: str = Field(description='Vertical units associated with geospatial_vertical_* attributes')
-    geospatial_vertical_positive: str = Field(description='Positive direction of geospatial_vertical_* attributes')
-    id: str = Field(description='filename without extension, needs to be globally unique w/ naming_authority')
-    institution: str = Field(description=f'The institute responsible for the creation of this data. Should read: {CREATOR_INSTITUTION}')
-    keywords: str = Field(description='A comma separated list of keywords from the GCMD')
-    keywords_vocabulary: str = Field(description=f'Controlled vocabulary for keywords. Should read {KEYWORDS_VOCABULARY}')
-    license: str = Field(description=f'The license for this data. Should read: {LICENSE}')
-    metadata_link: str = Field(description='A link to a release of a controlled vocabulary of metadata attributes')
-    naming_authority: str = Field(description=f'The authority responsible for naming this data. Should read {NAMING_AUTHORITY}')
-    platform: str = Field(description=f'Platform name of the FAAM aircraft. Should read: {PLATFORM}')
-    platform_type: str = Field(description='Platform type: aircraft')
-    project: str = Field(description='The project a flight was for. Expected to be of the form <project_acronym> - <project_name>')
-    publisher_email: str = Field(description='The email address of the data publisher', regex='.+@.+')
-    publisher_institution: str = Field(description=f'The institution responsible for publishing data. Should read: {PUBLISHER_INSTITUTION}')
-    publisher_type: str = Field(description=f'Type of entity responsible for publishing data. Should be {PUBLISHER_TYPE}')
-    publisher_url: str = Field(description=f'The URL of the data publisher. Should be {PUBLISHER_URL}')
-    references: str = Field(description='A list of references relevant to the file')
-    revision_date: datetime.datetime = Field(description='The date this revision created. Analogous to date_created')
-    revision_number: int = Field(description='The revision number of the dataset. Starts at 0, increments by 1 for each revision', ge=0)
-    source: str = Field(description='A description of the source of the data. An instrument where this makes sense, otherwise a description of how data obtained')
-    standard_name_vocabulary: str = Field(description='A vocabulary of standard names. Expected to be a CF standard name table, e.g. CF Standard Name Table v78', regex='CF Standard Name Table v[0-9]+')
-    summary: str = Field(description='A brief summary of the data')
-    time_coverage_duration: datetime.timedelta = Field(description='The duration of the data, in ISO8601 duration format')
-    time_coverage_start: datetime.datetime = Field(description='The start time of the data in the file, in ISO8601 format')
-    time_coverage_end: datetime.datetime = Field(description='The end time of the data in the file, in ISO8601 format')
-    title: str = Field(description='A brief title for the dataset')
-    uuid: UUID = Field(description='V3 UUID: MD5 hash of id+date_created e.g. 9bceaff5-9991-85c6-bca5-d8c0393dc60d')
+    Conventions: str = Field(
+        description="The conventions followed by this data",
+        example='CF-1.9 ACDD-1.3'
+    )
+
+    acknowledgement: str = Field(
+        description=f'The acknowledgement for use of this data. Should read: {ACKNOWLEDGEMENT} for FAAM data.',
+        example=ACKNOWLEDGEMENT
+    )
+
+    creator_address: str = Field(
+        description=f'The address of the FAAM offices. Should read: {CREATOR_ADDRESS} for FAAM data.',
+        example=CREATOR_ADDRESS
+    )
+
+    creator_email: str = Field(
+        description='The email address of the data creator, or a generic FAAM email.',
+        example='creator@faam.ac.uk'
+    )
+
+    creator_institution: str = Field(
+        description=f'The institute responsible for the creation of the data. Should read: {CREATOR_INSTITUTION} for FAAM data',
+        example=CREATOR_INSTITUTION
+    )
+
+    creator_name: str = Field(
+        description='The name of the person or institute responsible for this data',
+        example='A. N. Other'
+    )
+
+    creator_type: str = Field(
+        description=f'The type of creator. Should be one of {", ".join(CREATOR_TYPES)}',
+        example=CREATOR_TYPES[0]
+    )
+
+    date: datetime.date = Field(
+        description='Date of the flight',
+        example='1970-01-01T00:00:00Z'
+    )
+    
+    date_created: datetime.datetime = Field(
+        description='ISO:8601 date/time file created, ideally yyyy-mm-ddTHH:MM:SSZ. Equivalent to revision_date.',
+        example='1970-01-01T06:00:00Z'
+    )
+
+    flight_date: datetime.date = Field(
+        description='Date of the flight',
+        example='1970-01-01'
+    )
+
+    flight_number: str = Field(
+        description='The flight number', regex='[a-z][0-9]{3}',
+        example='a001'
+    )
+
+    geospatial_bounds: str = Field(
+        description='A well-known text representation of the flight envelope',
+        example=''
+    )
+
+    geospatial_bounds_crs: str = Field(
+        description='Coordinate reference system for geospatial attributes.', regex='EPSG:[0-9]+',
+        example='EPSG:4979'
+    )
+
+    geospatial_lat_max: float = Field(
+        ge=-90, le=90, description='Maximum latitude of the flight.',
+        example=60.0
+    )
+
+    geospatial_lat_min: float = Field(
+        ge=-90, le=90, description='Minimum latitude of the flight.',
+        example=50.0
+    )
+
+    geospatial_lat_units: str = Field(
+        description='The units for geospatial_lat_* attributes',
+        example='degree_north'
+    )
+
+    geospatial_lon_max: float = Field(
+        description='Maximum longitude of the flight', gt=-360, lt=360,
+        example=10.0
+    )
+
+    geospatial_lon_min: float = Field(
+        description='Minimum longitude of the flight', gt=-360, le=360,
+        example=-10.0
+    )
+
+    geospatial_lon_units: str = Field(
+        description='The units for geospatial_lon_* attributes',
+        example='degree_east'  
+    )
+
+    geospatial_vertical_max: float = Field(
+        description='The maximum altitude of the flight',
+        example=10000.0
+    )
+
+    geospatial_vertical_min: float = Field(
+        description='The minimum altitude of the flight',
+        example=100.0
+    )
+
+    geospatial_vertical_units: str = Field(
+        description='Vertical units associated with geospatial_vertical_* attributes',
+        example='m'
+    )
+
+    geospatial_vertical_positive: str = Field(
+        description='Positive direction of geospatial_vertical_* attributes',
+        example='up'
+    )
+
+    id: str = Field(
+        description='filename without extension, needs to be globally unique w/ naming_authority',
+        example='core[-type]_faam_<yyyymmdd>[_vNNN]_rN[_Nhz]'
+    )
+
+    institution: str = Field(
+        description=f'The institute responsible for the creation of this data. Should read: {CREATOR_INSTITUTION} for FAAM data.',
+        example=CREATOR_INSTITUTION
+    )
+
+    keywords: str = Field(
+        description='A comma separated list of keywords from the GCMD',
+        example='keyword1, keyword2'
+    )
+
+    keywords_vocabulary: str = Field(
+        description=f'Controlled vocabulary for keywords. Should read {KEYWORDS_VOCABULARY}',
+        example=KEYWORDS_VOCABULARY
+    )
+
+    license: str = Field(
+        description=f'The license for this data. Should read: {LICENSE}',
+        example=LICENSE
+    )
+
+    metadata_link: str = Field(
+        description='A link to a release of a controlled vocabulary of metadata attributes',
+        example='https://github.com/FAAM-146/faam-data/releases/tag/vX.Y'
+    )
+
+    naming_authority: str = Field(
+        description=f'The authority responsible for naming this data. Should read {NAMING_AUTHORITY}',
+        example=NAMING_AUTHORITY
+    )
+
+    platform: str = Field(
+        description=f'Platform name of the FAAM aircraft. Should read: {PLATFORM}',
+        example=PLATFORM
+    )
+
+    platform_type: str = Field(
+        description='Platform type: aircraft',
+        example='aircraft'
+    )
+
+    project: str = Field(
+        description='The project a flight was for. Expected to be of the form <project_acronym> - <project_name>',
+        example='PROJECT: A project with a long title'
+    )
+
+    publisher_email: str = Field(
+        description='The email address of the data publisher',
+        example='support@ceda.ac.uk'
+    )
+
+    publisher_institution: str = Field(
+        description=f'The institution responsible for publishing data. Should read: {PUBLISHER_INSTITUTION}',
+        example=PUBLISHER_INSTITUTION
+    )
+
+    publisher_type: str = Field(
+        description=f'Type of entity responsible for publishing data. Should be {PUBLISHER_TYPE}',
+        example=PUBLISHER_TYPE
+    )
+
+    publisher_url: str = Field(
+        description=f'The URL of the data publisher. Should be {PUBLISHER_URL}',
+        example=PUBLISHER_URL
+    )
+
+    references: str = Field(
+        description='A list of references relevant to the file',
+        example='https://www.faam.ac.uk https://www.ceda.ac.uk'
+    )
+
+    revision_date: datetime.datetime = Field(
+        description='The date this revision created. Analogous to date_created',
+        example='1970-01-01T06:00:00Z'
+    )
+
+    revision_number: int = Field(
+        description='The revision number of the dataset. Starts at 0, increments by 1 for each revision', ge=0,
+        example=0
+    )
+
+    source: str = Field(
+        description='A description of the source of the data. An instrument where this makes sense, otherwise a description of how data obtained',
+        example='description of how data obtained'
+    )
+
+    standard_name_vocabulary: str = Field(
+        description='A vocabulary of standard names. Expected to be a CF standard name table, e.g. CF Standard Name Table v78', regex='CF Standard Name Table v[0-9]+',
+        example='CF Standard Name Table v78'
+    )
+
+    summary: str = Field(
+        description='A brief summary of the data',
+        example='Data obtained on FAAM aircraft during flight a001'
+    )
+
+    time_coverage_duration: datetime.timedelta = Field(
+        description='The duration of the data, in ISO8601 duration format',
+        example='PT1H'
+    )
+
+    time_coverage_start: datetime.datetime = Field(
+        description='The start time of the data in the file, in ISO8601 format',
+        example='1970-01-01T00:00:00Z'
+    )
+
+    time_coverage_end: datetime.datetime = Field(
+        description='The end time of the data in the file, in ISO8601 format',
+        example='1970-01-01T01:00:00Z'
+    )
+
+    title: str = Field(
+        description='A brief title for the dataset',
+        example='Data from flight a001'
+    )
+    
+    uuid: UUID = Field(
+        description='V3 UUID: MD5 hash of id+date_created',
+        example='9bceaff5-9991-85c6-bca5-d8c0393dc60d'
+    )
 
     # Optional Globals
     calibration_date: Optional[datetime.date] = Field(description='Calibration date, where this applies to all file contents')
