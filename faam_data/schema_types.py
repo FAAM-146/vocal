b676d008ad94314815ecd3426a1c0caa2a4a0603
Dave Sproson <daspr@faam.ac.uk>
2022-01-06 16:54:08 +0000

    Added an inversion dict from np type to InfoType.

--- faam_data/schema_types.py
+++ faam_data/schema_types.py
@@ -1,5 +1,7 @@
 from typing import NewType, Union
 
+import numpy as np
+
 
 derived_str = lambda typ: f'<{typ}: derived_from_file>'
 DerivedType = NewType('DerivedType', str)
@@ -13,8 +15,22 @@ Numeric = Union[float, int]
 
 type_str = lambda typ: f'<{typ}>'
 InfoType = NewType('InfoType', str)
+Byte = InfoType(type_str('byte'))
+Integer8 = InfoType(type_str('int8'))
+Integer16 = InfoType(type_str('int16'))
 Integer32 = InfoType(type_str('int32'))
 Integer64 = InfoType(type_str('int64'))
 Float32 = InfoType(type_str('float32'))
 Float64 = InfoType(type_str('float64'))
 String = InfoType(type_str('string'))
+
+np_invert = {
+    np.dtype('float32'): Float32,
+    np.dtype('float64'): Float64,
+    np.dtype('int8'): Integer8,
+    np.dtype('int16'): Integer16,
+    np.dtype('int32'): Integer32,
+    np.dtype('int64'): Integer64,
+    np.dtype('float32'): Float32,
+    np.dtype('float64'): Float64
+}
