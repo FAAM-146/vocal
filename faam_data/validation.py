2046428fab644be3db1c0ae39bda52d32c99c234
Dave Sproson <daspr@faam.ac.uk>
2022-01-06 18:30:14 +0000

    Added some safety checks to substitute_placeholders val hack.

--- faam_data/validation.py
+++ faam_data/validation.py
@@ -20,19 +20,26 @@ re_validator = functools.partial(validator, allow_reuse=True)
 
 def substitute_placeholders(cls, values):
         for key, value in values.items():
+            
             if not isinstance(value, (str, list)):
                 continue
 
-            example = cls.schema()['properties'][key]['example']
+            try:
+                example = cls.schema()['properties'][key]['example']
+            except KeyError:
+                continue
+
             if 'derived' in value:
                 values[key] = example
+            
             if isinstance(value, list):
                 replaced = []
                 for i, list_val in enumerate(value):
-                    if 'derived' in list_val:
+                    if isinstance(list_val, str) and 'derived_from_file' in list_val:
                         replaced.append(example[i])
                     else:
-                        replaced.append[list_val]
+                        replaced.append(list_val)
+
                 values[key] = replaced
             
         return values
