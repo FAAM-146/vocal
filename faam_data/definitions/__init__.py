6723f34249a1380137e9db982b6bdc6e5d54289f
Dave Sproson <daspr@faam.ac.uk>
2022-01-07 17:29:54 +0000

    Define products with yaml rather than code.

--- faam_data/definitions/__init__.py
+++ faam_data/definitions/__init__.py
@@ -1,5 +1,9 @@
 from typing import Any
 
+import copy
+import pydantic
+import yaml
+
 from faam_data.attributes.globals import GlobalAttributes
 from faam_data.definitions import dimensions
 from faam_data.attributes import (
@@ -7,6 +11,7 @@ from faam_data.attributes import (
 )
 from faam_data.templates import required_variable, required_globals
 from ..variable import Variable
+from ..dataset import Dataset
 
 def make_1hz(var: Variable) -> Variable:
     """
@@ -47,3 +52,30 @@ def global_attribute_factory(**kwargs: Any) -> GlobalAttributes:
     for key, value in kwargs.items():
         setattr(attributes, key, value)
     return attributes
+
+def dataset_from_partial_yaml(yamlfile: str) -> pydantic.BaseModel:
+    def parse_definition(defn: dict, ctype: str='dataset') -> dict:    
+        for var in defn['variables']:
+            
+            _temp = copy.deepcopy(required_variable)
+            _temp.update(var['attributes'])
+            var['attributes'] = _temp
+
+        try:
+            for g in defn['groups']:
+                parse_definition(g, ctype='group')
+        except KeyError:
+            pass
+
+        if ctype == 'dataset':
+            _temp = copy.deepcopy(required_globals)
+            _temp.update(defn['attributes'])
+            defn['attributes'] = _temp
+
+        return defn
+        
+    with open(yamlfile, 'r') as f:
+        y = yaml.load(f, Loader=yaml.Loader)
+        dataset = Dataset.construct(**parse_definition(y['Dataset']))
+
+    return dataset
