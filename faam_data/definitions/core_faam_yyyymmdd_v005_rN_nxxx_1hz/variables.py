7cac313ca3a99077c5726c575fe917c0f175fc1e
Dave Sproson <daspr@faam.ac.uk>
2022-01-07 17:28:57 +0000

    Removed test 1hz definitions.

--- faam_data/definitions/core_faam_yyyymmdd_v005_rN_nxxx_1hz/variables.py
+++
@@ -1,7 +0,0 @@
-from faam_data.definitions import make_1hz
-from faam_data.definitions.core_faam_yyyymmdd_v005_rN_nxxx.variables import (
-    variables as v
-)
-
-
-variables = [make_1hz(i) for i in v]
