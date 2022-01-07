2ec10e7c2787d1baf80b7f57044fc8827a9ad684
Dave Sproson <daspr@faam.ac.uk>
2022-01-07 17:01:45 +0000

    Add netcdf creation hook to Group.

--- faam_data/group.py
+++ faam_data/group.py
@@ -23,5 +23,24 @@ class Group(BaseModel):
     dimensions: Optional[list[Dimension]]
     groups: Optional[list[Group]]
     variables: list[Variable]
+
+    def to_nc_container(self, nc):
+        this_group = nc.createGroup(self.meta.name)
+
+        if self.groups is not None:
+            for group in self.groups:
+                group.to_nc_container(this_group)
+
+        for var in self.variables:
+            var.to_nc_container(this_group)
+
+        for attr, value in self.attributes:
+            if value is None:
+                continue
+            
+            try:
+                setattr(this_group, attr, value)
+            except TypeError:
+                setattr(this_group, attr, str(value))
     
 Group.update_forward_refs()
