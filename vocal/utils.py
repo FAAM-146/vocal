4347384101e1c298f35e6d2f22badecfc03dc0ca
Dave Sproson <dave.sproson@faam.ac.uk>
2023-06-23 13:22:30 +0100

    SLight correction to get_type_From_placeholder.

--- vocal/utils.py
+++ vocal/utils.py
@@ -90,7 +90,7 @@ def get_type_from_placeholder(placeholder: str) -> tuple[str, str]:
             An info type, for example <str>, <float32>
         """
         # rex = re.compile("<([a-z0-9]+): derived_from_file>")
-        rex = re.compile('<(Array)?\[?([a-z0-9]+)\]?: derived_from_file>')
+        rex = re.compile('<(Array)?\[?([a-z0-9]+)\]?: derived_from_file\s?.*>')
         matches = rex.search(placeholder)
         if not matches:
             raise ValueError('Unable to get type from placeholder')
