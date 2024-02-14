135ea64d55ff353297b87207eab53172dee3cfd8
Dave Sproson <dave.sproson@ncas.ac.uk>
2024-02-14 17:45:59 +0000

    Add a mixin for dataset utils, which hooks vocal into specific vocal projects.

--- vocal/application/release.py
+++ vocal/application/release.py
@@ -42,6 +42,7 @@ def release(args: Namespace) -> None:
 
     try:
         module = import_project(project)
+        print(dir(module))
         defaults = module.defaults
         
     except ModuleNotFoundError as e:
